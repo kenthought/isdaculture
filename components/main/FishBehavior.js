adc979654cb566c84f460c1a2b86fbfa2d16bfd9
unknown <nananaupan@gmail.com>
2021-04-09 04:54:58 +0800

    added components in dashboard

---
+++ components/main/FishBehavior.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const FishBehavior = () => {
+    return (
+        <View>
+            <Text>Fish Behavior</Text>
+        </View>
+    )
+}
+
+export default FishBehavior
