a01a9f86cb6872dc1e593f825b05d5adb102f3fc
unknown <nananaupan@gmail.com>
2021-04-21 21:33:32 +0800

    added modal

--- components/main/FishBehavior.js
+++ components/main/FishBehavior.js
@@ -1,12 +1,61 @@
-import React from "react";
-import { View, StyleSheet, SafeAreaView, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, SafeAreaView, Text, TouchableOpacity } from "react-native";
+import Modal from "react-native-modal"
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
 
 export const FishBehavior = () => {
+    const [isModalVisible, setIsModalVisible] = useState(false)
+    
+    const FishBehaviorModal = () => (
+        <View>
+          <Modal
+            isVisible={isModalVisible}
+            onSwipeComplete={() => toggleModal()}
+            onBackdropPress={() => toggleModal()}
+            swipeDirection="down">
+            <View style={{ padding: 10, backgroundColor: "white", borderRadius: 20 }}>
+              <View style={{ marginVertical: 15, borderBottomWidth: 1, opacity: .3, marginHorizontal: 150 }} />
+              <View style={{ marginVertical: 7 }}>
+                <Text style={{ fontWeight: "bold", fontSize: 20 }} >Fish Behavior</Text>
+              </View>
+              <View style={{ marginVertical: 7 }}>
+                <Text style={{ fontWeight: "bold" }} >Normal </Text>
+                <Text>Temperature is greater than 34 but not more than 37</Text>
+              </View>
+              <View style={{ marginVertical: 7 }}>
+                <Text style={{ fontWeight: "bold" }} >Behaved</Text>
+                <Text>Temperature is greater than 34 but not more than 37</Text>
+              </View>
+              <View style={{ marginVertical: 7 }}>
+                <Text style={{ fontWeight: "bold" }} >Sluggish</Text>
+                <Text>Temperature is greater than 34 but not more than 37</Text>
+              </View>
+              <View style={{ marginVertical: 7 }}>
+                <Text style={{ fontWeight: "bold" }} >Stressed</Text>
+                <Text>Temperature is greater than 34 but not more than 37</Text>
+              </View>
+            </View>
+          </Modal>
+        </View>
+      )
+
+    const toggleModal = () => {
+        setIsModalVisible(!isModalVisible)
+      }
+
     return (
         <SafeAreaView style={styles.container}>
-            <View style={{ marginTop: 3, marginBottom: 8, justifyContent: "center" }}>
-                <Text style={styles.screenTitle}>Fish Behavior</Text>
+            <View style={{ flexDirection: "row", marginTop: 3, marginBottom: 8, justifyContent: "center" }}>
+                <View style={{ flex: 1 }}>
+                    <Text style={styles.screenTitle}>Fish Behavior</Text>
+                </View>
+                <View style={{ flex: .80, justifyContent: "center" }}>
+                    <TouchableOpacity onPress={toggleModal}>
+                        <MaterialCommunityIcons name="information-outline" size={26} />
+                    </TouchableOpacity>
+                </View>
             </View>
+            <FishBehaviorModal />
         </SafeAreaView>
     )
 }
